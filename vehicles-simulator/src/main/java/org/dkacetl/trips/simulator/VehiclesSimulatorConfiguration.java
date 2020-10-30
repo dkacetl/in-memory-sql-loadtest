bbac22b05933583651465593b7fb3d76d5a2f03c
dkacetl <dkacetl@gmail.com>
2020-10-30 23:09:32 +0100

    optimize properties

--- vehicles-simulator/src/main/java/org/dkacetl/trips/simulator/VehiclesSimulatorConfiguration.java
+++ vehicles-simulator/src/main/java/org/dkacetl/trips/simulator/VehiclesSimulatorConfiguration.java
@@ -1,12 +1,8 @@
 package org.dkacetl.trips.simulator;
 
-import org.dkacetl.trips.api.simulator.VehicleEventsProducer;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
 
 /**
  * Vehicles-simulator configuration
@@ -15,13 +11,6 @@ import org.springframework.context.annotation.Configuration;
  */
 @Configuration
 @EnableConfigurationProperties(VehiclesSimulatorProperties.class)
-@ConditionalOnProperty(prefix = "vehicles-simulator", value = "enabled", havingValue = "true")
+@Import(VehiclesSimulatorProducer.class)
 public class VehiclesSimulatorConfiguration {
-
-    private static final Logger LOGGER = LoggerFactory.getLogger(VehiclesSimulatorConfiguration.class);
-
-    @Bean
-    public VehicleEventsProducer vehicleEventsProducer() {
-        return new VehiclesSimulatorProducer();
-    }
 }
--- vehicles-simulator/src/main/java/org/dkacetl/trips/simulator/VehiclesSimulatorProperties.java
+++ vehicles-simulator/src/main/java/org/dkacetl/trips/simulator/VehiclesSimulatorProperties.java
@@ -5,8 +5,6 @@ import org.springframework.boot.context.properties.ConfigurationProperties;
 @ConfigurationProperties(prefix = "vehicles-simulator")
 public class VehiclesSimulatorProperties {
 
-    private boolean enabled = false; // in case of false, no simulator will be enabled
-
     private String type = "default"; //
 
     private int vehiclesCount = 10;  // count of simulated vehicles
@@ -14,14 +12,6 @@ public class VehiclesSimulatorProperties {
     private long emittingRate = 10L; // how often will be event emitted, milisec (common for all vehicles)
 
     private int eventLimit = -1; // no limit, unlimited emitting
-
-    public boolean isEnabled() {
-        return enabled;
-    }
-
-    public void setEnabled(boolean enabled) {
-        this.enabled = enabled;
-    }
 
     public String getType() {
         return type;
